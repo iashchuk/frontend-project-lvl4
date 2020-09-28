bc30fdd69a0f038287b6e46184849bedd55281e2
iashchuk <221box@list.ru>
2020-09-29 01:12:49 +0300

    Update app's settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
 // @ts-check
 
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-const isProduction = process.env.NODE_ENV === 'production';
-// const isDevelopment = !isProduction;
-console.log('isProduction', isProduction);
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -20,7 +16,7 @@ module.exports = {
     publicPath: '/assets/',
   },
   plugins: [
-    // new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin(),
   ],
   module: {
     rules: [
